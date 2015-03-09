682345a3f311d994d3acf0f5c9d9f97e7151cb62
SOB <seanobrien@Seans-MacBook-Pro.local>
2015-03-08 21:18:42 -0700

    ...

--- customSearch.js
+++ customSearch.js
@@ -3,13 +3,14 @@ document.getElementById('submitSearch').onclick = function() {
     //var url2 = "https://api.idxbroker.com/mls/zipcodes/b010?zipcode=92118";
     var url2 = "https://api.idxbroker.com/mls/zipcodes/b010?zipcode=92118";
 
-    $.ajax({type:"GET",
+    $.ajax({
+            type:'GET',
             beforeSend: function (request){
                 request.setRequestHeader('accesskey','tuyJ8N1ocyIITwfizYiwR6');
             },
             url: url2,
             contentType :'application/x-www-form-urlencoded',
-            dataType:'jsonp',
+            dataType:'application/json',
             success:function(msg){
                 console.log("Success");
                 console.log(msg);
