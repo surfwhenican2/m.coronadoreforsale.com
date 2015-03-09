1c2d8efe760e77dea96f01087eb941373d9e38ea
SOB <seanobrien@Seans-MacBook-Pro.local>
2015-03-08 21:39:50 -0700

    ...

--- customSearch.js
+++ customSearch.js
@@ -9,6 +9,7 @@ document.getElementById('submitSearch').onclick = function() {
             beforeSend: function (request){
                 request.setRequestHeader('accesskey','gY15jJpvJI_GyFnANULFsi');
                 request.setRequestHeader('ancillarykey','gY15jJpvJI_GyFnANULFsi');
+                request.setRequestHeader("Access-Control-Allow-Origin", "*");
             },
             url: url2,
             contentType :'application/x-www-form-urlencoded',
