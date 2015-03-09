39aafa776ca87a649c0aab54c71ff00e67aa1c55
SOB <seanobrien@Seans-MacBook-Pro.local>
2015-03-08 21:01:58 -0700

    ...

---
+++ customSearchBackup.js
@@ -0,0 +1,37 @@
+document.getElementById('submitSearch').onclick = function() {
+
+    //var url2 = "https://api.idxbroker.com/mls/zipcodes/b010?zipcode=92118";
+    var url2 = "https://api.idxbroker.com/mls/zipcodes/b010";
+
+    $.ajax({type:"GET",
+            beforeSend: function (request){
+                request.setRequestHeader('accesskey','tuyJ8N1ocyIITwfizYiwR6');
+            },
+            url: url2,
+            data: {},
+            contentType :'application/x-www-form-urlencoded',
+            dataType:'jsonp',
+            success:function(msg){
+                console.log("Success");
+                console.log(msg);
+
+            },
+            error:function(error){
+                console.log("Error");
+                console.log(error);
+            }
+        }).done(function(data){
+            console.log("Done");
+            console.log("Sample Data:",data);
+
+        });
+
+    function callbackFunction(data){
+        console.log("Done");
+        console.log("Sample Data:",data);
+        console.log(data);
+
+    }
+
+    return false;
+};
