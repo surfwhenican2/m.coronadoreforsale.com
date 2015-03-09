3b41e77e6339d55ab038046c613a739c083db0fa
SOB <seanobrien@Seans-MacBook-Pro.local>
2015-03-08 20:07:02 -0700

    Testing API Search

--- customSearch.js
+++ customSearch.js
@@ -1,7 +1,8 @@
 document.getElementById('submitSearch').onclick = function() {
     var minBeds = document.getElementById('minBeds').value;
     var maxPrice = document.getElementById('maxPrice').value;
-    var url = "http://search.coronadoreforsale.com/idx/results/listings?idxID=b010&pt=1&";
+    var url2 = "https://api.idxbroker.com/mls/zipcodes/b010?";
+    var url = "";
     url += "hp="+maxPrice;
     url += "&bd="+minBeds;
     var cays = document.getElementById("checkbox-cays").checked;
@@ -16,6 +17,24 @@ document.getElementById('submitSearch').onclick = function() {
     if (shores== true){
         url += "&a_community%5B%5D=CORONADO+SHORES"
     }
-    window.open(url,"_self");
+    //window.open(url,"_self");
+
+    $.ajax({type:"GET",
+            beforeSend: function (request){
+                request.setRequestHeader('accesskey','tuyJ8N1ocyIITwfizYiwR6');
+            },
+            url: url2,
+            data: {},
+            contentType :'application/x-www-form-urlencoded',
+            dataType:'json',
+            processData:false,
+            success:function(msg){
+                console.log(msg);
+            },
+            error:function(error){
+                console.log(error);
+            }
+        });
+
     return false;
 };
